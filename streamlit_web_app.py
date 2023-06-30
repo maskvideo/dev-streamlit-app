d5c2984204161f95bcf3ee0600b80029b88af31e
maskvideo <135969831+maskvideo@users.noreply.github.com>
2023-06-30 15:07:54 +0300

    Update streamlit_web_app.py

--- streamlit_web_app.py
+++ streamlit_web_app.py
@@ -132,7 +132,7 @@ if st.button("Mask video") and uploaded_file is not None:
             file_name=masked_video_filepath
         )
         
-        if st.button("Click here to free memory") and uploaded_file is not None:
+        st.button("Click here to free memory"):
             aws_client.delete_file(unmasked_video_name)
             aws_client.delete_file("masked_people.jpg")
             aws_client.delete_folder("unmasked_frames/")
