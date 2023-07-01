21c458b4afa976f4c9ff3a01f100a51a1706da33
maskvideo <135969831+maskvideo@users.noreply.github.com>
2023-07-01 18:16:44 +0300

    Update streamlit_web_app.py

--- streamlit_web_app.py
+++ streamlit_web_app.py
@@ -8,6 +8,7 @@ import aws_client
 
 global kernel_size
 global epsilon
+global fps
 
 
 def convert_bytes_to_opencv(bytes_image):
@@ -50,7 +51,7 @@ masked = retina.update_parameters(unmasked_pil_img, (5,5), 10, faces_locations)
 frames_files = []
 
 
-kernel_size = st.slider("Choose blurr", 0, 100)
+kernel_size = st.slider("Choose blur", 0, 100)
 epsilon = st.slider("Choose coverage", 0, 40)
 if st.button("Update"):
     slider_value = (kernel_size, epsilon)
@@ -63,8 +64,6 @@ if slider_value is not None:
         masked = retina.update_parameters(unmasked_pil_img, (kernel_size, kernel_size), epsilon, faces_locations)
         update_masked_image(masked)
 
-# Create a text input field for the video path
-video_path = st.text_input("Enter the path to the video file")
 
 # Force the text input field to lose focus
 st.write('')
@@ -76,24 +75,67 @@ uploaded_file = st.file_uploader("Choose a video file", type=["mp4"])
 if uploaded_file is not None:
     s3 = boto3.client('s3')
     with st.spinner('Uploading...'):
-        s3.upload_fileobj(uploaded_file, aws_client.BUCKET_NAME, uploaded_file.name)
+        unmasked_video_name = uploaded_file.name
+        s3.upload_fileobj(uploaded_file, aws_client.BUCKET_NAME, unmasked_video_name)
     st.write('Upload successful!')
 
-# Create a button to start processing the video
-# TODO: need to find a way to make this faster
-if st.button("Process video"):
-    with st.spinner("Extracting frames from video..."):
-        extract_frames.extract_frames_from_video(aws_client.get_video_url("world.mp4"))
 
-    # groups = [frames_files[i:i + 100] for i in range(0, len(frames_files), 100)]
+# Create a file uploader widget to allow the user to choose the file path for the masked video
+masked_video_filepath = st.text_input("Enter a file path for the masked video", value="masked_video.mp4")
 
 # TODO: convert this to work with s3
-if st.button("Mask video"):
-    frames_files = extract_frames.sorted_frames_files()
-    st.write("start mask")
+if st.button("Mask video") and uploaded_file is not None:
+    with st.spinner("Extracting frames from video..."):
+        fps = extract_frames.extract_frames_from_video(aws_client.get_video_url(unmasked_video_name))
+    frames_files = extract_frames.sorted_frames_files(aws_client.BUCKET_NAME, "unmasked_frames/")
+    st.write("Start masking the video. It might take a while...")
 
-    for frame in frames_files:
-        faces_locations = retina.all_faces_locations("frames_from_video" + "/" + frame)
-        retina.update_parameters("frames_from_video" + "/" + frame, frame, (kernel_size, kernel_size), epsilon, faces_locations)
+    masked_frames = []
 
-    st.write("end mask")
+    progress_bar = st.progress(0)
+
+
+    for idx, frame in enumerate(frames_files):
+        frame_image_s3 = aws_client.image_from_s3(aws_client.BUCKET_NAME, frame)
+        unmasked_frame_img = cv2.imdecode(np.frombuffer(frame_image_s3, np.uint8), cv2.IMREAD_COLOR)
+        faces_locations = retina.all_faces_locations(unmasked_frame_img)
+        masked_frames.append(retina.update_parameters(unmasked_frame_img, (kernel_size, kernel_size), epsilon, faces_locations))
+
+        # Update the progress bar
+        progress = (idx + 1) / len(frames_files)
+        progress_bar.progress(progress)
+    st.write("Masking completed. Go ahead and download the masked video!")
+
+    if masked_frames:
+        # Convert the PIL Image objects to NumPy arrays
+        masked_frames = [np.array(frame) for frame in masked_frames]
+
+        # Define the video codec and output parameters
+        fourcc = cv2.VideoWriter_fourcc(*"mp4v")
+        frame_size = (masked_frames[0].shape[1], masked_frames[0].shape[0])
+
+        # Create the video writer
+        video_writer = cv2.VideoWriter(masked_video_filepath, fourcc, fps, frame_size)
+
+        # Write the masked frames to the video file
+        for frame in masked_frames:
+            frame_np = np.array(frame)
+            video_writer.write(frame_np)
+
+        # Release the video writer
+        video_writer.release()
+
+        # File download button
+        st.download_button(
+            label="Download Masked Video",
+            data=open(masked_video_filepath, "rb").read(),
+            file_name=masked_video_filepath,
+            key='download_button'
+        )
+
+        # Deletion logic
+        if st.button("Please click here to free memory first") and uploaded_file is not None:
+            aws_client.delete_file(unmasked_video_name)
+            aws_client.delete_file("masked_people.jpg")
+            aws_client.delete_folder("unmasked_frames/")
+            st.write("Thank you! To mask another video please refresh the page.", unsafe_allow_html=True)
